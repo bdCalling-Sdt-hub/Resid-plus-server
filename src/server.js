045231ff892d3d28d121d197590d2fe26f19ae5d
towhidul-me <towhidul1999@gmail.com>
2023-09-26 09:39:14 +0600

    User password update bug solved

--- src/server.js
+++ src/server.js
@@ -3,10 +3,6 @@ require('dotenv').config();
 
 const port = process.env.PORT || 3001;
 
-<<<<<<< HEAD
-app.listen(port, '103.161.9.43',() => {
-=======
-app.listen(port, '192.168.10.18',() => {
->>>>>>> 892caac5b7a8b004420bcef27a0a154c44b5e38a
+app.listen(port, '192.168.10.15',() => {
   console.log(`Resid+ is listening on port ${port}`)
 });
