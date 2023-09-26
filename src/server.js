b1ff41549dc4d9d396acf3ec958664d668aba760
alshakibeelahi <alshakibeelahi@gmail.com>
2023-09-26 09:41:01 +0600

    ip change

--- src/server.js
+++ src/server.js
@@ -3,10 +3,6 @@ require('dotenv').config();
 
 const port = process.env.PORT || 3001;
 
-<<<<<<< HEAD
-app.listen(port, '103.161.9.43',() => {
-=======
 app.listen(port, '192.168.10.18',() => {
->>>>>>> 892caac5b7a8b004420bcef27a0a154c44b5e38a
   console.log(`Resid+ is listening on port ${port}`)
 });
