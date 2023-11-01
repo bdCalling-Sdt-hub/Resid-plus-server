fa0ccada59392e78d5a59aced8025ad1c4c19635
alshakibeelahi <alshakibeelahi@gmail.com>
2023-11-01 17:07:46 +0600

    image upload path changed

--- src/models/User.js
+++ src/models/User.js
@@ -23,7 +23,7 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: [true, 'Password must be given'], set: (v) => bcrypt.hashSync(v, bcrypt.genSaltSync(10)) },
   image: {
     type: Object, required: false, default: {
-      publicFileUrl: `http://${process.env.API_SEVER_IP}:3000/uploads/users/user-1695552693976.jpg`,
+      publicFileUrl: `${process.env.IMAGE_UPLOAD_BACKEND_DOMAIN}/uploads/users/user-1695552693976.jpg`,
       path: 'public\\uploads\\users\\user-1695552693976.jpg'
     }
   },
