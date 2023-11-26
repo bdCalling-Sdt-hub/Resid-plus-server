c681b3411b9da0fe4dad4bc96ffdb37fc88f34f0
alshakibeelahi <alshakibeelahi@gmail.com>
2023-11-26 09:55:39 +0600

    cancel booking route updated

--- src/routes/bookingRouter.js
+++ src/routes/bookingRouter.js
@@ -10,7 +10,7 @@ router.post('/calculate-time-and-amount', isValidUser, calculateTimeAndPrice)
 router.get('/:id', isValidUser, bookingDetails);
 router.get('/refund-policy/:id', isValidUser, refundPolicy);
 router.get('/', isValidUser, allBooking);
-router.put('/cancel-booking-by-user/:id',isValidUser, cancelBookingByUser);
+router.put('/:id/cancel-booking-by-user',isValidUser, cancelBookingByUser);
 router.put('/:id',isValidUser, updateBooking);
 router.get('/dashboard/ratio', isValidUser, bookingDashboardRatio);
 router.delete('/:id', isValidUser, deleteBooking);
